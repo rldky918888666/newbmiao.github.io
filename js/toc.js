ddb6f1f85338dcbdee116bbf9ce05fee6b3fc62e
newbmiao <newbvirgil@gmail.com>
2017-08-24 21:54:18 +0800

    Site updated: 2017-08-24 21:54:18

--- js/toc.js
+++ js/toc.js
@@ -1,3 +1,3 @@
-// build time:Sat May 20 2017 14:15:50 GMT+0800 (CST)
+// build time:Thu Aug 24 2017 21:54:07 GMT+0800 (CST)
 (function(){var e=function(e){var t=0,o=0;while(e.offsetParent){t+=e.offsetTop;o+=e.offsetLeft;e=e.offsetParent}return{top:t,left:o}};var t=function(){var e=document.documentElement.scrollTop||document.body.scrollTop;if(e>=i){n.style.position="fixed";n.style.top="10px";n.style.width="inherit"}else{n.style.position="static";n.style.width="auto"}};var o=document.querySelector(".toc");if(!o)return;var n=document.querySelector(".widget-toc");var r=n.querySelector(".container");r.appendChild(o.cloneNode(true));n.style.display="block";var i=e(n).top;if(document.addEventListener){document.addEventListener("scroll",t)}})();
 //rebuild by neat 
